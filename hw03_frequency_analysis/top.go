33904468dcfb42071efcbfa88f2261593dd2ba2b
romangricuk <gricuk.r@yandex.ru>
2024-04-28 14:05:32 +0300

    Hw03 frequency analysis (#3)
    * HW2 is completed
    
    * HW3 is completed
    
    * bugfix. Новые тест кейсы в TestUnpack. Убрано ограничение isLetter
    
    ---------
    
    Co-authored-by: gritsuk_r <gritsuk_r@rusklimat.ru>

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,40 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"cmp"
+	"slices"
+	"strings"
+)
+
+func Top10(str string) []string {
+	words := strings.Fields(str)
+
+	if len(words) == 0 {
+		return []string{}
+	}
+
+	wordsFreqMap := make(map[string]int)
+
+	for _, word := range words {
+		wordsFreqMap[word]++
+	}
+
+	words = make([]string, 0, len(wordsFreqMap))
+
+	for value := range wordsFreqMap {
+		words = append(words, value)
+	}
+
+	slices.SortFunc(words, func(a string, b string) int {
+		if wordsFreqMap[b] == wordsFreqMap[a] {
+			return cmp.Compare(a, b)
+		}
+		return cmp.Compare(wordsFreqMap[b], wordsFreqMap[a])
+	})
+
+	if len(words) > 10 {
+		return words[:10]
+	}
+
+	return words
 }

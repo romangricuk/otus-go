39122a075fd42387f57e3f6ffa2d4af0ae5856c8
gritsuk_r <gritsuk_r@rusklimat.ru>
2024-06-29 16:50:56 +0300

    HW11

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,10 +15,57 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type telnetClient struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &telnetClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+func (tc *telnetClient) Connect() error {
+	conn, err := net.DialTimeout("tcp", tc.address, tc.timeout)
+	if err != nil {
+		return fmt.Errorf("on connecting: %w", err)
+	}
+	tc.conn = conn
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (tc *telnetClient) Close() error {
+	if tc.conn != nil {
+		return tc.conn.Close()
+	}
+	return nil
+}
+
+func (tc *telnetClient) Send() error {
+	scanner := bufio.NewScanner(tc.in)
+	for scanner.Scan() {
+		_, err := tc.conn.Write(append(scanner.Bytes(), '\n'))
+		if err != nil {
+			return fmt.Errorf("on sending data: %w", err)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("on reading from input: %w", err)
+	}
+	return nil
+}
+
+func (tc *telnetClient) Receive() error {
+	_, err := io.Copy(tc.out, tc.conn)
+	if err != nil {
+		return fmt.Errorf("on receiving data: %w", err)
+	}
+	return nil
+}
